efbe8f103318ebd3cb83dcbd76b68ac484adc545
eric <xsephiroth@foxmail.com>
2020-10-31 17:04:14 +0800

    ImgCard

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
   }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
 `;
 
 function App() {
@@ -17,18 +20,52 @@ function App() {
       <GlobalStyle />
       <Layout>
         <Grid cols={2}>
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/paper-mario-and-the-origami-king/712x400_PMO_GameStoreHalf_v01.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/animal-crossing-new-horizons/712x400_ACNH_GameStoreHalf_v01.jpg" />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/paper-mario-and-the-origami-king/712x400_PMO_GameStoreHalf_v01.jpg"
+            text="Available now"
+            text2X
+            esrbLogo
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/animal-crossing-new-horizons/712x400_ACNH_GameStoreHalf_v01.jpg"
+            text="Available now"
+            text2X
+            esrbLogo
+          />
         </Grid>
         <Grid cols={4}>
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/pokemon-sword-and-shield/354x198_PSword_GameStoreQuarter_v01.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/pokemon-sword-and-shield/354x198_PShield_GameStoreQuarter_v01.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/fortnite-nexus-war/354x198_gs_fortnite_nexuswar.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/super-smash-bros-ultimate/354x198_Smash_GameStoreQuarter_v02.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/breath-of-the-wild/354x198_ZeldaBreathofWild_v01.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/super-mario-party/354x198_SuperMarioParty_goldPoints_GameStoreQuarter_noRibbon.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/xenoblade-chronicles/354x198_GameStoreQuarter_x.jpg" />
-          <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/club-house-games/354x198__GameStoreQuarter_games.jpg" />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/pokemon-sword-and-shield/354x198_PSword_GameStoreQuarter_v01.jpg"
+            text="DLC available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/pokemon-sword-and-shield/354x198_PShield_GameStoreQuarter_v01.jpg"
+            text="DLC available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/fortnite-nexus-war/354x198_gs_fortnite_nexuswar.jpg"
+            text="New Season"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/super-smash-bros-ultimate/354x198_Smash_GameStoreQuarter_v02.jpg"
+            text="Available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/breath-of-the-wild/354x198_ZeldaBreathofWild_v01.jpg"
+            text="Available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/super-mario-party/354x198_SuperMarioParty_goldPoints_GameStoreQuarter_noRibbon.jpg"
+            text="Available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/xenoblade-chronicles/354x198_GameStoreQuarter_x.jpg"
+            text="Available now"
+          />
+          <ImgCard
+            src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/cta-tile/switch/club-house-games/354x198__GameStoreQuarter_games.jpg"
+            text="Available now"
+          />
         </Grid>
         <ImgCard src="https://www.nintendo.com/content/dam/noa/en_US/merchandising/curated-list/switch/new-releases/1180x500_NewReleases_Switch_7_2020_v01.jpg" />
       </Layout>
--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,15 +1,62 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledContainer = styled.div``;
+const StyledContainer = styled.div`
+  position: relative;
+`;
 
 const StyledImg = styled.img`
+  display: inline-block;
   width: 100%;
 `;
 
-const ImgCard = ({ src }) => {
+const StyledOverlay = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 5px;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  color: white;
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.7) 70%,
+    rgba(0, 0, 0, 0) 100%
+  );
+
+  padding-top: 0.5rem;
+  padding-left: 0.8rem;
+  line-height: 2em;
+
+  ${(props) =>
+    props.text2X &&
+    css`
+      font-size: 1.8em;
+    `}
+`;
+
+const EsrbLogo = styled.img.attrs({
+  src: "https://www.nintendo.com/content/dam/noa/en_US/esrb/e/e.svg",
+})`
+  position: absolute;
+  right: 1em;
+  bottom: 1em;
+
+  width: 3.2em;
+`;
+
+const ImgCard = ({ src, text, text2X = false, esrbLogo = false }) => {
   return (
     <StyledContainer>
       <StyledImg src={src} />
+      {!!text && (
+        <StyledOverlay text2X={text2X}>
+          <span>{text}</span>
+        </StyledOverlay>
+      )}
+      {esrbLogo && <EsrbLogo />}
     </StyledContainer>
   );
 };
